41eb515f624b5f46275de3bda4222f059b4e371e
Tom Wayson <tom@tomwayson.com>
2016-04-13 23:55:51 -0700

    added basemap toggle

--- src/mapService.js
+++ src/mapService.js
@@ -2,6 +2,8 @@ import declare from 'dojo/_base/declare';
 import Map from 'esri/Map';
 import MapView from 'esri/views/MapView';
 import FeatureLayer from 'esri/layers/FeatureLayer';
+import BasemapToggle from 'esri/widgets/BasemapToggle';
+import BasemapToggleVM from 'esri/widgets/BasemapToggle/BasemapToggleViewModel';
 
 // define a stateful service to manage the map
 const MapService = declare([], {
@@ -13,6 +15,21 @@ const MapService = declare([], {
     delete options.basemap;
     options.map = this.map;
     this.view = new MapView(options);
+  },
+
+  createBasemapToggle (node, secondaryBasemap) {
+    if (!node || !secondaryBasemap || !this.view) {
+      return;
+    }
+
+    return new BasemapToggle({
+      // Setting widget properties via viewModel is subject to
+      // change for the 4.0 final release
+      viewModel: new BasemapToggleVM({
+        view: this.view,
+        secondaryBasemap: secondaryBasemap
+      })
+    }, node);
   },
 
   // add a feature layer to the map
