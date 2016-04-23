5941cc81d49157ae7d50cc10f5ed58346899b7b6
Tom Wayson <tom@tomwayson.com>
2016-04-23 14:52:18 -0700

    Update 2016-04-23T21:52:15.074Z

--- app/nls/strings.js
+++ app/nls/strings.js
@@ -2,6 +2,6 @@
 define({
   root: {
     About: 'About',
-    sidePanelMessage: 'This application is implemented as Dojo widgets written in ES2015 bundled using Rollup.js.'
+    sidePanelMessage: 'This application is implemented as Dojo widgets written in ES2015 bundled and minified using Rollup.js.'
   }
 });
